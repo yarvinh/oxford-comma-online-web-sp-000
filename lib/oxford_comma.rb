15f0b766b0e951f3798e8e5d73a018ea22090bb4
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 15:12:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.length < 3
+  array.join(" and ")
+else
+ and_array = []
+ and_array.push(array[-2],array[-1])
+  array.pop
+  array.pop
+  string_comma = array.join(", ")
+  string_and = and_array.join(", and ")
+   return "#{string_comma}, #{string_and}"
+end
 end
